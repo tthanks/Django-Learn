9ad991e6feabe88012a5461aa51cbd2fbf2b7d0d
tthanks <tthankstang@outlook.com>
2019-05-06 23:06:08 +0800

    Initial commit

---
+++ users/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import url
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+
+
+from . import views
+app_name = 'users'
+
+urlpatterns = [
+    # 登录页面
+    url(r'^login/$', LoginView.as_view(template_name='users/login.html'), name='login'),
+    # 注销
+    url(r'^logout/$', LogoutView.as_view(), name='logout'),
+
+    # 注册页面
+    url(r'^register/$', views.register, name='register'),
+
+]
